183e7b5c0cab6eabef5d7aaa7ba26b22e1ad669f
hjuliano2004 <112650184+hjuliano2004@users.noreply.github.com>
2025-08-06 18:32:02 -0300

    implementa proprio-hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const [lista, setLista] = useState([]);
+
+  useEffect(()=>{
+    let url = "localhost:3000/equipamentos";
+
+    async function fetchData(){
+      
+      try{
+
+       let requisicao = await fetch(url);
+       let json = await requisicao.json();
+       setLista(json);
+
+
+      }catch(e){
+      console.error(e);
+      }
+
+    }
+
+    fetchData();
+  }, []);
 
 
   return (
     
+      <main>
+
+        <div id="btn">
+          <Link>listagem(Cards)"</Link>
+          <Link>Listagem(tabela)</Link>
+        </div>
+
+
       <Routes>
         
       </Routes>
+      </main>
     
   )
 }
