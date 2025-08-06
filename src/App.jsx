b8e3ae78cbec4b0d1123b6094fde96a7f68c90ea
hjuliano2004 <112650184+hjuliano2004@users.noreply.github.com>
2025-08-06 17:32:36 -0300

    projeto base iniciado, roteamento configurado

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -5,9 +6,9 @@ function App() {
 
   return (
     
-      <div>
-        <div>aaaaaaaaaaaa</div>
-      </div>
+      <Routes>
+        
+      </Routes>
     
   )
 }
